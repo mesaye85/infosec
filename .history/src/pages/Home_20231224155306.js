16b1bdd542873e21a561290e527d3206e836bf53
Mesaye Addisu <addisumesaye@gmail.com>
2023-12-24 15:53:49 -0500

    compile error correction

--- .history/src/pages/Home_20231224155306.js
+++ .history/src/pages/Home_20231224155306.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from '../components/Navbar'; // Adjust the path as per your project structure
 import Articles from '../components/Articles';
 import MostViewed from '../components/MostViewed';
+
 import './Home.css'; // Make sure the path to your CSS file is correct
 
 const Home = () => {
